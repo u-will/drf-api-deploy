affca70342c85c56c8bcee735145083df833ec21
Ulrich Wilfried Motchoffo Kom <motchoffow@gmail.com>
2021-01-03 17:33:28 -0800

    adds some extra files

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+#read .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3p^9!42%9og$p7pd1&f_b2(2zpt5#ziea@8@mqu6i-wchpt&)1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env.str('SECRET_KEY')
 
-ALLOWED_HOSTS = [] # sometimes you'll need to add localhost here
+DEBUG = env.bool('DEBUG')
 
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 # Application definition
 
